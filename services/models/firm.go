a46567fe214333262aac810e8b8ccf7646f3b9e2
Chuuka Ro(Hafrans) <hafrans@163.com>
2020-06-19 00:07:30 +0800

    add employer model

--- services/models/firm.go
+++ services/models/firm.go
@@ -12,20 +12,20 @@ type Category struct {
 // 申请调解人
 type Applicant struct {
 	Model
-	Name           string
-	Birthday       utils.Time
-	Nationality    string
-	IdentityNumber string
-	Contact        string
-	Address        string
+	Name           string     `json:"applicant_name" form:"applicant_name" gorm:"type:varchar(128);not null"`
+	Birthday       utils.Time `json:"applicant_birth" form:"applicant_birth"`
+	Nationality    string     `json:"applicant_nationality" form:"applicant_nationality" gorm:"type:varchar(32);not null"`
+	IdentityNumber string     `json:"applicant_id" form:"applicant_id" gorm:"type:varchar(20);not null;unique_index"`
+	Contact        string     `json:"applicant_contact" form:"applicant_contact" gorm:"type:varchar(32)"`
+	Address        string     `json:"applicant_address" form:"applicant_address" gorm:"type:varchar(255)"`
 }
 
 // 用人单位
 type Employer struct {
 	Model
-	Name                    string
-	LegalRepresentative     string
-	UniformSocialCreditCode string
-	Contact                 string
-	Address                 string
+	Name                    string `json:"employer_name" form:"employer_name" gorm:"type:varchar(255);not null;unique_index"`
+	LegalRepresentative     string `json:"employer_faren" form:"employer_faren" gorm:"type:varchar(128);not null"`
+	UniformSocialCreditCode string `json:"employer_uscc" form:"employer_uscc" gorm:"type:varchar(32);not null;unique_index"`
+	Contact                 string `json:"employer_contact" form:"employer_contact" gorm:"type:varchar(32)"`
+	Address                 string `json:"employer_address" form:"employer_address" gorm:"type:varchar(255)"`
 }
